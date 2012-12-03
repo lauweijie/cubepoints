772108fd281e53af5c12bcfc0b65a93f0bc38e85
Jonathan Lau <jon@jon.sg>
2012-12-03 19:15:05 +0800

    adding in other random files

---
+++ modules/test.mod.php
@@ -0,0 +1,18 @@
+<?php
+
+class Test extends CubePointsModule {
+
+	public static $module = array(
+		'name' => 'Test Module',
+		'version' => '1.0',
+		'module_uri' => 'http://jon.sg/testmodule',
+		'author_name' => 'Jonathan',
+		'author_uri' => 'http://jon.sg',
+		'description' => 'This is a test module.'
+	);
+
+	public function main() {
+	
+	}
+
+}
