486372ecbd811a7a779cd175df2fc00ed0a84823
Jonathan Lau <jon@jon.sg>
2012-12-05 14:02:38 +0800

    finished the modules system. sorting by name

---
+++ modules/cat.mod.php
@@ -0,0 +1,45 @@
+<?php
+
+class Zee extends CubePointsModule {
+
+	public static $module = array(
+		'name' => 'Zee Module',
+		'version' => '1.0',
+		'module_uri' => 'http://jon.sg/testmodule',
+		'author_name' => 'Jonathan',
+		'author_uri' => 'http://jon.sg',
+		'description' => 'zzzzzzzzzzzzz'
+	);
+
+	public function main() {
+	
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- views/admin_modules.php
+++ views/admin_modules.php
@@ -7,16 +7,22 @@ if ( ! function_exists( 'add_action' ) )
 	<div id="icon-plugins" class="icon32"></div>
 	<h2>
 		<?php _e('CubePoints', 'cubepoints'); ?> <?php _e('Modules', 'cubepoints'); ?>
-		<a href="http://cubepoints.com/forums/forum/modules-releases/" class="add-new-h2"><?php _e('Get More Modules', 'cubepoints'); ?></a>
+		<a href="http://cubepoints.com/forums/forum/modules-releases/?utm_source=plugin&utm_medium=more_modules&utm_campaign=cubepoints" class="add-new-h2"><?php _e('Get More Modules', 'cubepoints'); ?></a>
 	</h2>
+
+	<?php if( $_GET['activate'] == 'true' ) : ?>
+		<div id="message" class="updated"><p><?php _e( 'Module', 'cubepoints' ); ?> <strong><?php _e( 'activated', 'cubepoints' ); ?></strong>.</p></div>
+	<?php elseif( $_GET['deactivate'] == 'true' ) : ?>
+		<div id="message" class="updated"><p><?php _e( 'Module', 'cubepoints' ); ?> <strong><?php _e( 'deactivated', 'cubepoints' ); ?></strong>.</p></div>
+	<?php endif; ?>
 
 	<?php
 		$currUri = $_SERVER[REQUEST_URI];
 		$currUri = remove_query_arg( '_wpnonce', $currUri );
 		$currUri = remove_query_arg( 'action', $currUri );
 		$currUri = remove_query_arg( 'module', $currUri );
-		$redirUri = remove_query_arg( 'activate', $currUri );
-		$redirUri = remove_query_arg( 'deactivate', $currUri );
+		$currUri = remove_query_arg( 'activate', $currUri );
+		$currUri = remove_query_arg( 'deactivate', $currUri );
 		$modules = $this->availableModules();
 		$activeModules = array();
 		$inactiveModules = array();
@@ -41,13 +47,13 @@ if ( ! function_exists( 'add_action' ) )
 		
 	?>
 
-	<?php if( count($modules) > 0 ): ?>
+	<?php if( count($modules) > 0 ) : ?>
 		<ul class="subsubsub">
 			<li class="all"><a href="<?php echo add_query_arg('module_status', 'all', $currUri); ?>"<?php echo ($moduleStatus == 'all') ? ' class="current"' : ''; ?>><?php _e('All', 'cubepoints'); ?> <span class="count">(<?php echo count($modules); ?>)</span></a></li>
-			<?php if( count($activeModules) > 0 ): ?>
+			<?php if( count($activeModules) > 0 ) : ?>
 				<li class="active">| <a href="<?php echo add_query_arg('module_status', 'active', $currUri); ?>"<?php echo ($moduleStatus == 'active') ? ' class="current"' : ''; ?>><?php _e('Active', 'cubepoints'); ?> <span class="count">(<?php echo count($activeModules); ?>)</span></a></li>
 			<?php endif; ?>
-			<?php if( count($inactiveModules) > 0 ): ?>
+			<?php if( count($inactiveModules) > 0 ) : ?>
 			<li class="inactive">| <a href="<?php echo add_query_arg('module_status', 'inactive', $currUri); ?>"<?php echo ($moduleStatus == 'inactive') ? ' class="current"' : ''; ?>><?php _e('Inactive', 'cubepoints'); ?> <span class="count">(<?php echo count($inactiveModules); ?>)</span></a></li>
 			<?php endif; ?>
 		</ul>
@@ -67,21 +73,32 @@ if ( ! function_exists( 'add_action' ) )
 			</tr>
 		</tfoot>
 		<tbody>
-			<?php if( count($modulesToDisplay) == 0 ): ?>
+			<?php if( count($modulesToDisplay) == 0 ) : ?>
 				<tr class="no-items"><td class="colspanchange" colspan="2"><?php _e('You do not appear to have any modules available at this time.', 'cubepoints'); ?></td></tr>
-			<?php else: ?>
-				<?php foreach ( $modulesToDisplay as $module ) : ?>
+			<?php else : ?>
+
 				<?php
-					$moduleDetails = $this->moduleDetails( $module );
+					$modulesToDisplayDetails = array();
+					$modulesToDisplayNames = array();
+					foreach ( $modulesToDisplay as $module ) {
+						$moduleDetails = $this->moduleDetails( $module );
+						$modulesToDisplayDetails[ $module ] = $moduleDetails;
+						$modulesToDisplayNames[] = $moduleDetails['name'];
+					}
+					array_multisort($modulesToDisplayNames, $modulesToDisplayDetails);
+				?>
+
+				<?php foreach ( $modulesToDisplayDetails as $module => $moduleDetails ) : ?>
+				<?php
 					$moduleActivated = $this->moduleActivated( $module );
 				?>
 					<tr id="<?php echo $module; ?>" class="<?php echo $moduleActivated ? 'active' : 'inactive'; ?>">
 						<td class="plugin-title">
 							<strong><?php echo $moduleDetails['name']; ?></strong>
 							<div class="row-actions-visible">
-								<?php if( ! $moduleActivated ): ?>
+								<?php if( ! $moduleActivated ) : ?>
 									<span class="activate"><a href="<?php echo wp_nonce_url( add_query_arg( array( 'action' => 'activate_module', 'module' => $module ) ), 'activate_module_' . $module ); ?>" title="<?php _e('Activate this module', 'cubepoints'); ?>" class="edit"><?php _e('Activate', 'cubepoints'); ?></a></span>
-								<?php else: ?>
+								<?php else : ?>
 									<span class="deactivate"><a href="<?php echo wp_nonce_url( add_query_arg( array( 'action' => 'deactivate_module', 'module' => $module ) ), 'deactivate_module_' . $module ); ?>" title="<?php _e('Deactivate this module', 'cubepoints'); ?>" class="edit"><?php _e('Deactivate', 'cubepoints'); ?></a></span>
 								<?php endif; ?>
 							</div>
@@ -90,12 +107,12 @@ if ( ! function_exists( 'add_action' ) )
 							<div class="plugin-description"><p><?php echo $moduleDetails['description']; ?></p></div>
 							<div class="second plugin-version-author-uri">
 								<?php _e('Version', 'cubepoints'); ?> <?php echo $moduleDetails['version']; ?> |
-								<?php if( ! empty ( $moduleDetails['author_uri'] ) ): ?>
+								<?php if( ! empty ( $moduleDetails['author_uri'] ) ) : ?>
 									<?php _e('By', 'cubepoints'); ?> <a href="<?php echo $moduleDetails['author_uri']; ?>" title="<?php _e('Visit author homepage', 'cubepoints'); ?>"><?php echo $moduleDetails['author_name']; ?></a>
-								<?php else: ?>
+								<?php else : ?>
 									<?php _e('By', 'cubepoints'); ?> <?php echo $moduleDetails['author_name']; ?>
 								<?php endif; ?>
-								<?php if( ! empty ( $moduleDetails['module_uri'] ) ): ?>
+								<?php if( ! empty ( $moduleDetails['module_uri'] ) ) : ?>
 									| <a href="<?php echo $moduleDetails['module_uri']; ?>" title="<?php _e('Visit module site', 'cubepoints'); ?>">Visit module site</a>
 								<?php endif; ?>
 							</div>
